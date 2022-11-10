f2b91beecfd6edb0af92d97937c19aec9852076f
ArshKedia <64605104+ArshKedia@users.noreply.github.com>
2022-11-10 12:25:12 +0530

    Add files via upload

---
+++ bigspicy.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python3
+# vim: set shiftwidth=2 softtabstop=2 ts=2 expandtab:
+#
+#    Copyright 2022 Google LLC
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import os
+import re
+import optparse
+from optparse import OptionParser
+import glob
+
+import circuit
+import circuit_writer
+import spice
+import spice_analyser
+from design import Design
+
+
+def PrefixRelativeName(prefix, name):
+  if name.startswith('/'):
+    return name
+  return os.path.join(prefix, name)
+
+
+def FilesExistOrError(file_names):
+  for file_name in file_names:
+    if not os.path.exists(file_name):
+      raise IOError(f'File not found: {file_name}')
+
+
+def RequireOptions(options, *argv):
+  for arg in argv:
+    if not getattr(options, arg):
+      raise Exception(
+          f'option missing: {arg}; the following options are required: ' +
+          ', '.join(argv))
+
+
+def DefineOptions(optparser):
+  optparser.add_option('-t', '--top', dest='top_name', default=None, help='top module')
+  optparser.add_option('--verilog', dest='verilog_files', default=[], action='append', help='verilog files')
+  optparser.add_option('--verilog_include', dest='verilog_includes', default=[], action='append', help='verilog include paths')
+  optparser.add_option('--verilog_defines', dest='verilog_defines', default=[], action='append', help='verilog macro definitions')
+  optparser.add_option('--spef', dest='spef_files', default=[], action='append', help='spef files')
+  
+  # FIXME: what is the difference between these two "spice" options? 
+  optparser.add_option('--spice', dest='spice_files', default=[], action='append', help='read spice file contents. Subcircuits are read to circuit.Modules')
+  optparser.add_option('--spice_header', dest='spice_header_files', default=[], action='append', help='read spice file headers. Subcircuits are read for port order and stored as ExternalModules')
+
+  optparser.add_option('-s', '--dump_spice', dest='dump_spice', default=None, action='store', help='big spice file to write out')
+  optparser.add_option('--test_manifest', dest='test_manifest', default=None, action='store', help='read this test manifest and try to find and add the results')
+  optparser.add_option('--test_analysis', dest='test_analysis', default=None, action='store', help='read this analysis proto and try to find and add the results')
+
+  optparser.add_option('-f', '--flatten_spice', dest='flatten_spice', default=False, action='store_true', help='flatten spice decks as much as possible')
+  optparser.add_option('-d', '--working_dir', dest='working_dir', default=None, action='store', help='prefix directory to output all files')
+
+  optparser.add_option('--delays_csv', dest='delays_csv', default=None, action='store', help='write CSV of measured delays')
+  optparser.add_option('--input_caps_csv', dest='input_caps_csv', default='input_caps.csv', action='store', help='write CSV of measured delays')
+
+  # TODO(growly): Helper options.
+  optparser.add_option('--import', dest='import_circuit', default=False, action='store_true', help='import a circuit from verilog, SPEF, spice, etc')
+  optparser.add_option('--load', dest='load', default=None, action='store', help='read circuit proto containing netlist')
+  optparser.add_option('--save', dest='save', default=None, action='store', help='write circuit proto containing final netlist to this file')
+  optparser.add_option('--show', dest='show_design', default=False, action='store_true', help='print summary of loaded design')
+
+  optparser.add_option('--from_port', dest='from_port', default=None, action='store', help='dump passively-connected path from this port (requires --to_port)')
+  optparser.add_option('--to_port', dest='to_port', default=None, action='store', help='dump passively-connected path to this port (requires --from_port)')
+
+  optparser.add_option('--generate_input_capacitance_tests',
+                       dest='generate_input_capacitance_tests',
+                       default=False,
+                       action='store_true',
+                       help='generate spice tests for external module input capacitances')
+  optparser.add_option('--analyze_input_capacitance_tests',
+                       dest='analyze_input_capacitance_tests',
+                       default=False,
+                       action='store_true',
+                       help='')
+
+  optparser.add_option('--generate_module_tests',
+                       dest='generate_module_tests',
+                       default=False,
+                       action='store_true',
+                       help='')
+  # TODO(growly): Normalize to incorrect English spelling.
+  optparser.add_option('--analyze_module_tests',
+                       dest='analyze_module_tests',
+                       default=False,
+                       action='store_true',
+                       help='')
+
+
+def GlobAndFlatten(files):
+  flattened = []
+  for spec in files:
+    matched = glob.glob(spec)
+    if not matched:
+      flattened.append(spec)
+      continue
+    flattened.extend(matched)
+  return flattened
+
+
+def Main():
+  optparser = OptionParser()
+  DefineOptions(optparser)
+  options, args = optparser.parse_args()
+  return WithOptions(options)
+
+
+def WithOptions(options: optparse.Values):
+
+  # Make any output directories necessary.
+  output_directory = '/home/arsh/BigSpicy'
+  #output_directory = options.working_dir or '.'
+  #if not os.path.exists(output_directory):
+  #  os.makedirs(output_directory)
+  output_directory = os.path.abspath(output_directory)
+
+  # Check that input files exist.
+  verilog_files = GlobAndFlatten(options.verilog_files)
+  spef_files = GlobAndFlatten(options.spef_files)
+  spice_headers = GlobAndFlatten(options.spice_header_files)
+  spice_files = GlobAndFlatten(options.spice_files)
+  file_names = options.verilog_files + options.spef_files + spice_headers + spice_files
+
+  if options.test_manifest is not None:
+    file_names.append(options.test_manifest)
+  if options.test_analysis is not None:
+    file_names.append(options.test_analysis)
+  if options.load is not None:
+    file_names.append(options.load)
+
+  FilesExistOrError(file_names)
+
+  spice_libs = [os.path.abspath(path) for path in spice_headers]
+
+  design = Design()
+
+  if options.load:
+    # Read an existing circuit description (netlist) from disk.
+    reader = circuit_writer.CircuitWriter(design)
+    reader.ReadProtoToDesign(options.load)
+  elif options.import_circuit:
+    if spice_headers:
+      design.ParseSpiceDefinitions(spice_headers, headers_only=True)
+
+    if spice_files:
+      design.ParseSpiceDefinitions(spice_files, headers_only=False)
+
+    # TODO(growly): It would be nice to be able to add information from verilog,
+    # SPEF, spice, etc, files to an existing circuit description. By which I mean,
+    # it would be nice to be sure that works.
+    if verilog_files:
+      design.ParseVerilog(verilog_files, options.verilog_includes, options.verilog_defines)
+
+    if spef_files:
+      design.ParseSPEF(spef_files)
+
+    # Turn references to modules by name into references by pointer.
+    design.Link()
+    design.CheckPowerAndGround()
+
+  if options.show_design:
+    design.Show()
+
+  # Find top.
+  if options.top_name:
+    top = design.FindTop(options.top_name)
+    if top is None:
+      raise Exception(f'top not found: {options.top_name}')
+      sys.exit(1)
+
+  analyser = spice_analyser.SpiceAnalyser(design, output_directory, spice_libs)
+
+  if options.generate_input_capacitance_tests:
+    analyser.AddInputCapacitanceTestsForExternalModules(used_by_module=top)
+    analyser.WriteMetadata(options.test_manifest, options.test_analysis)
+
+  if options.analyze_input_capacitance_tests:
+    RequireOptions(options, 'test_manifest', 'test_analysis')
+    # Read results from a spice run.
+    analyser.FindInputCapacitances(options.test_manifest, options.test_analysis)
+    csv_file = PrefixRelativeName(
+        output_directory, options.input_caps_csv) if options.input_caps_csv else None
+    analyser.DumpInputCapacitances(csv_file)
+
+  def SplitBusIndex(text):
+    # s0[0] -> (s0, 0)
+    # s0 -> (s0, None)
+    # None -> None
+    if text is None:
+      return None
+    # Bus delimiter: []
+    match = re.match(r'(.*)\[(\d+)\]', text)
+    if match is None:
+      return text, None
+    return match.group(1), match.group(2)
+
+  # TODO(growly): Need to generalise VerilogIdentifier into something that can
+  # specify a bus index, width, etc, then use that as the argument here
+  # to allow users to specific bus pins as ports.
+  if options.from_port and options.to_port:
+    ignore_signals = set(['VGND', 'VPWR'])
+    from_port, from_index = SplitBusIndex(options.from_port)
+    if from_port in top.ports:
+      start_port = top.ports[from_port]
+    else:
+      raise Exception(f'port not found in {top.name}: {from_port}')
+      sys.exit(1)
+    from_index = int(from_index) if from_index is not None else 0
+
+    to_port, to_index = SplitBusIndex(options.to_port)
+    if to_port in top.ports:
+      stop_port = top.ports[to_port]
+    else:
+      raise Exception(f'port not found in {top.name}: {to_port}')
+      sys.exit(1)
+    to_index = int(to_index) if to_index is not None else 0
+
+    region = circuit.Module.FindConnectedRegionBetweenPorts(
+        start_port, stop_port,
+        (from_index, from_index), (to_index, to_index),
+        #probe_signal_names=set(['VPWR', 'VGND']),
+        ignore_signals=ignore_signals)
+    suffix = '_flat' if options.flatten_spice else ''
+    region.name = (f'{start_port.signal.name}_{from_index}_{from_index}_'
+                   f'{stop_port.signal.name}_{to_index}_{to_index}{suffix}')
+
+    file_name = f'{region.name}.sp'
+    full_path = os.path.join(output_directory, file_name)
+
+    spice_writer = spice.SpiceWriter(design, flatten=options.flatten_spice)
+    spice_writer.WriteRegion(full_path, region)
+
+  if options.generate_module_tests:
+    # TODO(growly): Move within SpiceAnalyser.
+    # Find a reasonable seed wire for the search.
+    #ignore_signals = set(self.design.power_net_names + self.design.ground_net_names)
+    ignore_signals = set(['GND', 'VSS', 'VDD'])
+    seed = None
+    for port in top.ports.values():
+      if port.signal in ignore_signals:
+        continue
+      seed = circuit.Wire(port.signal, 0)
+    assert(seed is not None)
+
+    #seed = circuit.Wire(top.ports['a'].signal, 0)
+    seed = circuit.Wire(list(top.ports.values())[0].signal, 0)
+
+    analyser.LoadRegionList(
+        circuit.Module.ExtractPassiveRegions(seed, ignore_signals))
+
+    analyser.AddRegionTests()
+    analyser.AddModuleTest(top, use_regions=True)
+
+    analyser.WriteMetadata(options.test_manifest, options.test_analysis)
+
+  if options.analyze_module_tests:
+    RequireOptions(options, 'test_manifest', 'test_analysis')
+    csv_file = PrefixRelativeName(
+        output_directory, options.delays_csv) if options.delays_csv else None
+    analyser.AnalyseModuleTests(
+        options.test_manifest,
+        options.test_analysis,
+        csv_file)
+
+  if options.save is not None:
+    writer = circuit_writer.CircuitWriter(design)
+    save_file = PrefixRelativeName(output_directory, options.save)
+    writer.WriteDesignToProto(save_file)
+    writer.WriteDesignToTextProto(save_file + '.txt')
+
+  if options.dump_spice is not None:
+    spice_writer = spice.SpiceWriter(design, flatten=options.flatten_spice)
+    spice_writer.WriteTop(
+        PrefixRelativeName(output_directory, options.dump_spice))
+
+
+if __name__ == '__main__':
+  Main()
---
+++ circuit_writer.py
@@ -0,0 +1,386 @@
+# vim: set shiftwidth=2 softtabstop=2 ts=2 expandtab:
+#
+#    Copyright 2022 Google LLC
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from google.protobuf import text_format
+
+import pdb
+import circuit
+import circuit_pb2 as circuit_pb
+import utils_pb2 as utils_pb
+from spice_util import SIUnitPrefix
+
+class CircuitWriter():
+
+  CIRCUIT_TO_PB_PORT_DIRECTION_MAP = {
+      circuit.Port.Direction.INPUT: circuit_pb.Port.Direction.INPUT,
+      circuit.Port.Direction.OUTPUT: circuit_pb.Port.Direction.OUTPUT,
+      circuit.Port.Direction.INOUT: circuit_pb.Port.Direction.INOUT,
+      circuit.Port.Direction.NONE: circuit_pb.Port.Direction.NONE,
+  }
+
+  PB_TO_CIRCUIT_PORT_DIRECTION_MAP = {
+    v: k for k, v in CIRCUIT_TO_PB_PORT_DIRECTION_MAP.items()
+  }
+
+  CIRCUIT_TO_PB_SI_PREFIX_MAP = {
+      None: None,
+      SIUnitPrefix.YOCTO: utils_pb.SIPrefix.YOCTO,
+      SIUnitPrefix.ZEPTO: utils_pb.SIPrefix.ZEPTO,
+      SIUnitPrefix.ATTO: utils_pb.SIPrefix.ATTO,
+      SIUnitPrefix.FEMTO: utils_pb.SIPrefix.FEMTO,
+      SIUnitPrefix.PICO: utils_pb.SIPrefix.PICO,
+      SIUnitPrefix.NANO: utils_pb.SIPrefix.NANO,
+      SIUnitPrefix.MICRO: utils_pb.SIPrefix.MICRO,
+      SIUnitPrefix.MILLI: utils_pb.SIPrefix.MILLI,
+      SIUnitPrefix.CENTI: utils_pb.SIPrefix.CENTI,
+      SIUnitPrefix.DECI: utils_pb.SIPrefix.DECI,
+      SIUnitPrefix.DECA: utils_pb.SIPrefix.DECA,
+      SIUnitPrefix.HECTO: utils_pb.SIPrefix.HECTO,
+      SIUnitPrefix.KILO: utils_pb.SIPrefix.KILO,
+      SIUnitPrefix.MEGA: utils_pb.SIPrefix.MEGA,
+      SIUnitPrefix.GIGA: utils_pb.SIPrefix.GIGA,
+      SIUnitPrefix.TERA: utils_pb.SIPrefix.TERA,
+      SIUnitPrefix.PETA: utils_pb.SIPrefix.PETA,
+      SIUnitPrefix.EXA: utils_pb.SIPrefix.EXA,
+      SIUnitPrefix.ZETTA: utils_pb.SIPrefix.ZETTA,
+      SIUnitPrefix.YOTTA: utils_pb.SIPrefix.YOTTA,
+  }
+
+  PB_TO_CIRCUIT_SI_PREFIX_MAP = {
+    v: k for k, v in CIRCUIT_TO_PB_SI_PREFIX_MAP.items()
+  }
+
+  def __init__(self, design):
+    self.design = design
+
+  @staticmethod 
+  def ToPortDirection(direction):
+    try:
+      return CircuitWriter.CIRCUIT_TO_PB_PORT_DIRECTION_MAP[direction]
+    except KeyError:
+      raise Exception(f'Unknown port direction: {direction}')
+
+  @staticmethod
+  def ToSIPrefix(prefix):
+    try:
+      return CircuitWriter.CIRCUIT_TO_PB_SI_PREFIX_MAP[prefix]
+    except KeyError:
+      raise Exception(f'Unknown SI prefix: {prefix}')
+
+  @staticmethod
+  def ToSignal(signal, signal_pb):
+    signal_pb.name = signal.name
+    signal_pb.width = signal.width
+
+  @staticmethod
+  def ToSlice(internal_slice, slice_pb):
+    slice_pb.signal = internal_slice.signal.name
+    slice_pb.top = internal_slice.top
+    slice_pb.bot = internal_slice.bottom
+
+  @staticmethod
+  def ToPort(port, port_pb):
+    port_pb.signal = port.signal.name
+    port_pb.direction = CircuitWriter.ToPortDirection(port.direction)
+
+  @staticmethod
+  def ToExternalModule(module, module_pb):
+    module_pb.name.name = module.name
+    for port_name in module.port_order:
+      if port_name not in module.ports:
+        raise RuntimeError(
+            f'port named in port order without associated Port object: {port_name}')
+      port_pb = module_pb.ports.add()
+      port = module.ports[port_name]
+      CircuitWriter.ToPort(port, port_pb)
+    for _, signal in module.signals.items():
+      CircuitWriter.ToSignal(signal, module_pb.signals.add())
+
+  @staticmethod
+  def ToParameter(name, value, param_pb):
+    param_pb.name = name
+    if isinstance(value, circuit.NumericalValue):
+      actual_value = value.value
+      if value.unit is not None:
+        param_pb.value.prefixed.prefix = CircuitWriter.ToSIPrefix(value.unit)
+        store_pb = param_pb.value.prefixed
+      else:
+        store_pb = param_pb.value
+      if isinstance(actual_value, float):
+        store_pb.double = actual_value
+      elif isinstance(actual_value, int) or isinstance(actual_value, long):
+        store_pb.value.prefixed.integer = actual_value
+      else:
+        raise Exception(f'Unknown numerical type: {type(value)} for {value}')
+    elif isinstance(value, str):
+      param_pb.value.string = value
+    else:
+      raise Exception(f'Unknown value type: {type(value)} for {value}')
+    return param_pb
+
+  @staticmethod
+  def ToConnection(port_name, connection, conn_pb):
+    conn_pb.portname = port_name
+    if connection.signal is not None:
+      conn_pb.target.sig = connection.signal.name
+    elif connection.slice is not None:
+      CircuitWriter.ToSlice(connection.slice, conn_pb.target.slice)
+    elif connection.concat is not None:
+      raise Exception(f'Don\'t know how to map concats in conncections: {connection}')
+    else:
+      raise Exception(f'Don\'t know how to map connection: {connection}')
+    return conn_pb
+
+  @staticmethod
+  def ToInstance(instance, instance_pb):
+    instance_pb.name = instance.name
+    instance_pb.module.local = instance.module_name
+    for name, value in instance.parameters.items():
+      CircuitWriter.ToParameter(name, value, instance_pb.parameters.add())
+    for port_name, connection in instance.connections.items():
+      CircuitWriter.ToConnection(
+          port_name, connection, instance_pb.connections.add())
+
+  def ToModule(module, module_pb):
+    module_pb.name = module.name
+    for name, value in module.default_parameters.items():
+      CircuitWriter.ToParameter(name, value, module_pb.parameters.add())
+    for port_name in module.port_order:
+      port = module.ports[port_name]
+      CircuitWriter.ToPort(port, module_pb.ports.add())
+    for name, signal in module.signals.items():
+      CircuitWriter.ToSignal(signal, module_pb.signals.add())
+    for name, instance in module.instances.items():
+      CircuitWriter.ToInstance(instance, module_pb.instances.add())
+
+  def ToCircuitProto(self):
+    design = self.design
+
+    package_pb = circuit_pb.Package()
+    #package_pb.name = design.top
+    for name, module in design.external_modules.items():
+      CircuitWriter.ToExternalModule(module, package_pb.ext_modules.add())
+    for name, module in design.known_modules.items():
+      CircuitWriter.ToModule(module, package_pb.modules.add())
+
+    return package_pb
+
+  def WriteDesignToTextProto(self, filename):
+    package = self.ToCircuitProto()
+    with open(filename, 'w') as f:
+      f.write(text_format.MessageToString(package))
+
+  def WriteDesignToProto(self, filename):
+    package = self.ToCircuitProto()
+    with open(filename, 'wb') as f:
+      # TODO(growly): bytes-to-string conversion required encoding!
+      f.write(package.SerializeToString())
+
+  @staticmethod 
+  def FromPortDirection(direction):
+    try:
+      return CircuitWriter.PB_TO_CIRCUIT_PORT_DIRECTION_MAP[direction]
+    except KeyError:
+      raise Exception(f'Unknown port direction: {direction}')
+
+  @staticmethod
+  def GetKnownSignal(signal_name, known_signals={}):
+    if signal_name not in known_signals:
+      raise Exception(f'Signal name is not known: {signal_name}')
+    return known_signals[signal_name]
+
+  @staticmethod
+  def FromSignal(signal_pb):
+    signal_name = signal_pb.name
+    return circuit.Signal(signal_name, width=signal_pb.width)
+
+  @staticmethod
+  def FromSlice(slice_pb, known_signals={}):
+    sliceyboi = circuit.Slice()
+    signal_name = slice_pb.signal
+    sliceyboi.signal = CircuitWriter.GetKnownSignal(signal_name, known_signals)
+    sliceyboi.top = slice_pb.top
+    sliceyboi.bottom = slice_pb.bot
+    return sliceyboi
+
+  @staticmethod
+  def FromPort(port_pb, known_signals={}):
+    # Ports represent signals implicitly
+    port = circuit.Port()
+    port.signal = CircuitWriter.GetKnownSignal(port_pb.signal, known_signals)
+    port.direction = CircuitWriter.FromPortDirection(port_pb.direction)
+    return port
+
+  @staticmethod
+  def FromConcat(concat_pb):
+    raise NotImplementedError()
+
+  @staticmethod
+  def FromModule(module_pb):
+    module = circuit.Module()
+    module.name = module_pb.name
+    for signal_pb in module_pb.signals:
+      signal = CircuitWriter.FromSignal(signal_pb)
+      if signal.name in module.signals:
+        print('how did this happen')
+        assert(signal.width == module.signals[signal.name].width)
+      module.signals[signal.name] = signal
+    for port_pb in module_pb.ports:
+      port = CircuitWriter.FromPort(port_pb, module.signals)
+      port.signal.Connect(port)
+      port_name = port.signal.name
+      module.ports[port_name] = port
+      module.port_order.append(port_name)
+      # TODO(growly): Add signals of port to signals? It should have been put there
+      # by the serialiser?
+    for instance_pb in module_pb.instances:
+      instance = CircuitWriter.FromInstance(instance_pb, module.signals)
+      module.instances[instance.name] = instance
+    for param_pb in module_pb.parameters:
+      name = param_pb.name
+      instance.default_parameters[name] = CircuitWriter.FromParameter(param_pb)
+
+    return module
+
+  @staticmethod
+  def FromSIPrefix(prefix_pb):
+    try:
+      return CircuitWriter.PB_TO_CIRCUIT_SI_PREFIX_MAP[prefix_pb]
+    except KeyError:
+      raise Exception('Unknown SI prefix: {prefix_pb}')
+
+  @staticmethod
+  def FromParameter(param_pb):
+    set_value = param_pb.value.WhichOneof('value')
+    if set_value is None:
+      return
+    if set_value in ('integer', 'double'):
+      value = getattr(param_pb.value, set_value)
+      return circuit.NumericalValue(value)
+    if set_value == 'string':
+      return param_pb.value.string
+    if set_value == 'prefixed':
+      prefixed_pb = param_pb.value.prefixed
+      set_inner_value = prefixed_pb.WhichOneof('number')
+      if set_inner_value in ('integer', 'double'):
+        # This is a numerical value.
+        value = getattr(param_pb.value, set_value)
+        prefix = CircuitWriter.FromSIPrefix(prefixed_pb.prefix)
+        return circuit.NumericalValue(value, prefix)
+    raise Exception(f'Cannot interpret Parameter {param_pb}')
+
+  @staticmethod
+  def FromConnection(port_name, conn_pb, known_signals={}):
+    connection = circuit.Connection(port_name)
+    referenced_signals = []
+    target = conn_pb.target
+    set_field = target.WhichOneof('stype')
+    if set_field is None:
+      return None
+    if set_field == 'sig':
+      connection.signal = CircuitWriter.GetKnownSignal(conn_pb.target.sig, known_signals)
+      referenced_signals.append(connection.signal)
+    elif set_field == 'slice':
+      connection.slice = CircuitWriter.FromSlice(conn_pb.target.slice, known_signals)
+      connection.slice.Connect(connection)
+      referenced_signals.append(connection.slice.signal)
+    elif set_field == 'concat':
+      raise Exception('Can\'t deal with concat types yet')
+    else:
+      raise Exception(f'Unknown field set in Connection proto: {set_field}')
+    return connection, referenced_signals
+
+  @staticmethod
+  def FromInstance(instance_pb, known_signals={}):
+    instance = circuit.Instance()
+    instance.name = instance_pb.name
+    set_field = instance_pb.module.WhichOneof('to')
+    if set_field == 'local':
+      instance.module_name = instance_pb.module.local
+    elif set_field == 'external':
+      instance.module_name = instance_pb.module.external.name
+    else:
+      raise Exception('Instance does not have module reference set')
+    for param_pb in instance_pb.parameters:
+      name = param_pb.name
+      instance.parameters[name] = CircuitWriter.FromParameter(param_pb)
+    for conn_pb in instance_pb.connections:
+      port_name = conn_pb.portname
+      connection, _ = CircuitWriter.FromConnection(port_name, conn_pb, known_signals)
+      connection.instance = instance
+      slice_or_signal = connection.signal or connection.slice
+      slice_or_signal.Connect(connection)
+      instance.connections[port_name] = connection
+    return instance
+
+  @staticmethod
+  def FromExternalModule(module_pb):
+    module = circuit.ExternalModule()
+    if module_pb.name.domain:
+      print('warning: external module has qualifying domain that we ignore: '
+            f'{module.name.domain}')
+    module.name = module_pb.name.name
+    for signal_pb in module_pb.signals:
+      signal = CircuitWriter.FromSignal(signal_pb)
+      module.signals[signal.name] = signal
+    for port_pb in module_pb.ports:
+      port = CircuitWriter.FromPort(port_pb, module.signals)
+      port.signal.Connects(port)
+      port_name = port.signal.name
+      module.ports[port_name] = port
+      module.port_order.append(port_name)
+    return module
+
+  def FromCircuitProto(self, package_pb):
+    design = self.design
+    for module_pb in package_pb.modules:
+      module = CircuitWriter.FromModule(module_pb)
+      design.known_modules[module.name] = module
+    for module_pb in package_pb.ext_modules:
+      module = CircuitWriter.FromExternalModule(module_pb)
+      if module.name in circuit.PRIMITIVE_MODULES:
+        continue
+      design.external_modules[module.name] = module
+
+    for module_name, module in design.known_modules.items():
+      for instance_name, instance in module.instances.items():
+        instance_of = instance.module_name
+        referenced = None
+        if instance_of in circuit.PRIMITIVE_MODULES:
+          referenced = circuit.PRIMITIVE_MODULES[instance_of]
+          # TODO(growly): This might not be true for all primitive modules.
+          referenced.is_passive = True
+        elif instance_of in design.known_modules:
+          referenced = design.known_modules[instance_of]
+        elif instance_of in design.external_modules:
+          referenced = design.external_modules[instance_of]
+        else:
+          raise Exception(
+              f'Instance {instance_name} of module {module_name} refers '
+              f'instantiates unknown module {instance_of}')
+        instance.module = referenced
+
+
+  def ReadProtoToDesign(self, filename):
+    package_pb = circuit_pb.Package()
+
+    with open(filename, 'rb') as f:
+      package_pb.ParseFromString(f.read())
+
+    self.FromCircuitProto(package_pb)
+
+
+
